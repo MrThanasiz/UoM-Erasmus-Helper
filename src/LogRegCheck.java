28715d7b349dd91f1a05b9b5595fdd6261071a65
Romputer <constadine.petrides@gmail.com>
2019-05-12 16:48:56 +0300

    Added method for checking if a user already exists in the database

--- src/LogRegCheck.java
+++ src/LogRegCheck.java
@@ -1,5 +1,34 @@
+import java.util.ArrayList;
 
 public class LogRegCheck {
 
+	private static boolean initializedList = false;
+	private static ArrayList<String> users;
 	
+	
+	public static void initializeUsersList() {
+		
+		//users = CentralRegistry.getUsers();
+		initializedList = true;
+	}
+	
+	
+	/*
+	 * Instructions:
+	 * 		if (LogRegCheck.checkExistingRegistry == null)
+	 * 			Initialize users list first.
+	 * 		else if ( (boolean) LogRegCheck.checkExistingRegistry == true )
+	 * 			User already exists.
+	 * 		else
+	 * 			User does not exist.
+	 */
+	public static Object checkExistingRegistry(String aRegister) {
+		
+		if ( !initializedList )
+			return null;
+		
+		if ( users.contains(aRegister) )
+			return true;
+		return false;
+	}
 }
