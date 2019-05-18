fa6d787be1666624df092229baf343082211b6f0
Novvator <stamatis.sellas@yahoo.com>
2019-05-18 21:01:13 +0300

    Added Unis Class, Added method in Secretariat

--- src/Secretariat.java
+++ src/Secretariat.java
@@ -1,8 +1,15 @@
+import java.util.ArrayList;
 
 public class Secretariat extends User{
 
 	public Secretariat(String username, String password) {
 		super(username, password);
+	}
+	
+	public ArrayList<Integer> get_workswith_List() {
+		ArrayList<Integer> lis = new ArrayList<Integer>();
+		lis.add(1);
+		return lis;
 	}
 
 }
