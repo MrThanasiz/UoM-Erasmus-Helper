deaecbb00c8341bd87a627f511f57b142277ef64
Novvator <stamatis.sellas@yahoo.com>
2019-05-21 02:53:40 +0300

    Minor improvements in University

--- src/University.java
+++ src/University.java
@@ -6,22 +6,26 @@ public class University {
 	private String town;
 	private String country;
 	private String language;
-	private ArrayList<Integer> workswith;
+	private ArrayList<Integer> coop;
+	private boolean langRequired;
 	
-	public University(String name, String town, String country, String language, ArrayList<Integer> workswith) {
-		
+	public University(String name, String town, String country, String language, ArrayList<Integer> coop,
+			boolean langRequired) {
 		this.name = name;
 		this.town = town;
 		this.country = country;
 		this.language = language;
-		this.workswith = workswith;
+		this.coop = coop;
+		this.langRequired = langRequired;
 	}
 
 	public String getLanguage() {
 		return language;
 	}
 	
-	
+	public ArrayList<Integer> getCoop() {
+		return coop;
+	}
 	
 
 }
