4842b7b097d32e19c111baa15fca0c19f6e3d566
Novvator <stamatis.sellas@yahoo.com>
2019-05-29 13:46:18 +0300

    Added createUnis() method in Main

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,54 @@
-import java.io.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
 		
+		
 	
 	}
+	
+	public void createUnis() {
+		
+		String name;
+		String town;
+		String country;
+		String language;
+		boolean langRequired;
+		int i=0;
+		ArrayList<Integer> coop = null;
+		coop.add(1);
+		File f = new File("Unis.txt");
+		try {
+			
+			Scanner sc = new Scanner(f);
+			while(sc.hasNext()) {
+				
+				name = sc.next();
+				town = sc.next();
+				country = sc.next();
+				language = sc.next();
+				langRequired = sc.nextBoolean();
+				
+				University un = new University(name,town,country,language,coop,langRequired);
+				i++;
+				
+			}
+			
+		}
+		
+		catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		
+	}
+
+	
 }
+
 
 
 
