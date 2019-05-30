7eee6966c2920cec60dde9264c750b345d432af3
MrThanasiz <8089997+MrThanasiz@users.noreply.github.com>
2019-05-30 17:09:31 +0300

    Progress on ValdationCheck, Progress on UniversitySelectFrame

---
+++ src/UniversitySelectFrame.java
@@ -0,0 +1,71 @@
+import javax.swing.*;    
+import java.awt.event.*;    
+public class UniversitySelectFrame { 
+	//Frame Options
+	private static final int WIDTH=1280;
+	private static final int HEIGHT=720;
+	
+	//"Choose Country" Label
+	private static final int L_X=WIDTH*21/40;
+	private static final int L_Y=HEIGHT*1/10;
+	
+	//Country+Score Dropdowns
+	private static final int C_X=WIDTH*8/20;
+	private static final int C_WIDTH=WIDTH*3/20;
+	private static final int C_HEIGHT=HEIGHT*1/20;
+	private static final int C1_Y=HEIGHT*1/10;
+	private static final int C2_Y=HEIGHT*2/10;
+	private static final int C3_Y=HEIGHT*3/10;
+	
+	//Next button
+	private static final int NB_X=WIDTH*6/10;
+	private static final int NB_Y=HEIGHT*5/10;
+	private static final int NB_WIDTH=WIDTH*1/10;
+	private static final int NB_HEIGHT=HEIGHT*1/20;
+	
+	
+	
+	JFrame f;    
+	UniversitySelectFrame(){
+		//Frame Options
+	    f=new JFrame("Epilogh Panepisthmiwn");
+	    f.setLayout(null);    
+	    f.setSize(WIDTH,HEIGHT);    
+	    
+	    
+	    //"Choose Country" Label
+	    final JLabel label = new JLabel();          
+	    label.setHorizontalAlignment(JLabel.TRAILING);  
+	    label.setSize(L_X,L_Y);  
+	    label.setText("Epelekse 1 ews 3 panepistimia");
+	    f.add(label);
+	    
+	    //Country+Score Dropdowns
+	    String languages[]={"University of Czechia","University of Denmark","University of Italy","University of Netherlands","University of Poland","University of Portugal","University of Spain"};
+	    //Todo ^^ getcountries list,calcucalte score
+	    final JComboBox country1=new JComboBox(languages);    
+	    country1.setBounds(C_X,C1_Y,C_WIDTH,C_HEIGHT); 
+	    f.add(country1);
+	    final JComboBox country2=new JComboBox(languages);    
+	    country2.setBounds(C_X,C2_Y,C_WIDTH,C_HEIGHT);
+	    f.add(country2);
+	    final JComboBox country3=new JComboBox(languages);    
+	    country3.setBounds(C_X,C3_Y,C_WIDTH,C_HEIGHT); 
+	    f.add(country3);
+	    
+	    //Next button
+	    JButton buttonNext=new JButton("Get Tips");  
+	    buttonNext.setBounds(NB_X,NB_Y,NB_WIDTH,NB_HEIGHT);
+	    f.add(buttonNext);
+	    buttonNext.addActionListener(new ActionListener() {  
+	        public void actionPerformed(ActionEvent e) {
+	        	//tipsFrame();
+	        	//^^uncomment this when tipsFrame() is ready.
+	        }
+	    });
+	        
+	    f.setVisible(true);  
+	     
+	               
+	}
+}
--- src/ValidationCheck.java
+++ src/ValidationCheck.java
@@ -1,6 +1,23 @@
 
 public class ValidationCheck {
-	public static boolean CheckUsername() {
-		return false;
+	public static boolean CheckID(int id) {
+		if (id>10000&&id<=99999)
+			return true;
+		else
+			return false;
 	}
+	public static boolean CheckAVG(double avg) {
+		if (avg>=5 && avg<=10)
+			return true;
+		else
+			return false;
+	}
+	public static boolean CheckFailed(int failed) {
+		if (failed>=0&&failed<=99)
+			return true;
+		else
+			return false;
+	}
+	
+	
 }
