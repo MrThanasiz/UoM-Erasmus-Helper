2bb821093dd50f3d4386f8503487a90741187c8d
MrThanasiz <8089997+MrThanasiz@users.noreply.github.com>
2019-06-03 03:14:05 +0300

    InputDataFrame: Completed Student: Added get/set for Scores UniversitySelectFrame: minor label changes ValidationCheck:changed inputs to String to make sure they're convertable

--- src/UniversitySelectFrame.java
+++ src/UniversitySelectFrame.java
@@ -1,58 +1,88 @@
 import javax.swing.*;    
-import java.awt.event.*;    
+import java.awt.event.*;
+import java.util.HashMap;    
 public class UniversitySelectFrame { 
 	//Frame Options
 	private static final int WIDTH=1280;
 	private static final int HEIGHT=720;
 	
-	//"Choose Country" Label
-	private static final int L_X=WIDTH*21/40;
-	private static final int L_Y=HEIGHT*1/10;
+	//"Choose university" Label
+	private static final int L_X=WIDTH*52/100;
+	private static final int L_Y=HEIGHT*10/100;
 	
-	//Country+Score Dropdowns
-	private static final int C_X=WIDTH*8/20;
-	private static final int C_WIDTH=WIDTH*3/20;
-	private static final int C_HEIGHT=HEIGHT*1/20;
-	private static final int C1_Y=HEIGHT*1/10;
-	private static final int C2_Y=HEIGHT*2/10;
-	private static final int C3_Y=HEIGHT*3/10;
+	//University 1,2,3 Labels
+	private static final int CL_X=WIDTH*39/100;
+	private static final int CL1_Y=HEIGHT*25/100;
+	private static final int CL2_Y=HEIGHT*45/100;
+	private static final int CL3_Y=HEIGHT*65/100;
+	//University+Score Drop-downs
+	private static final int C_X=WIDTH*40/100;
+	private static final int C_WIDTH=WIDTH*15/100;
+	private static final int C_HEIGHT=HEIGHT*5/100;
+	private static final int C1_Y=HEIGHT*10/100;
+	private static final int C2_Y=HEIGHT*20/100;
+	private static final int C3_Y=HEIGHT*30/100;
 	
 	//Next button
-	private static final int NB_X=WIDTH*6/10;
-	private static final int NB_Y=HEIGHT*5/10;
-	private static final int NB_WIDTH=WIDTH*1/10;
-	private static final int NB_HEIGHT=HEIGHT*1/20;
+	private static final int NB_X=WIDTH*60/100;
+	private static final int NB_Y=HEIGHT*50/100;
+	private static final int NB_WIDTH=WIDTH*10/100;
+	private static final int NB_HEIGHT=HEIGHT*5/100;
 	
 	
 	
 	JFrame f;    
-	UniversitySelectFrame(){
+	UniversitySelectFrame(Student stud){
 		//Frame Options
 	    f=new JFrame("University Select");
 	    f.setLayout(null);    
 	    f.setSize(WIDTH,HEIGHT);    
 	    
 	    
-	    //"Choose Country" Label
-	    final JLabel label = new JLabel();          
-	    label.setHorizontalAlignment(JLabel.TRAILING);  
-	    label.setSize(L_X,L_Y);  
-	    label.setText("Select 1 to 3 Universities");
-	    f.add(label);
+	    //"Choose University" Label
+	    final JLabel cunilabel = new JLabel();          
+	    cunilabel.setHorizontalAlignment(JLabel.TRAILING);  
+	    cunilabel.setSize(L_X,L_Y);  
+	    cunilabel.setText("Select 1 to 3 Universities");
+	    f.add(cunilabel);
 	    
-	    //Country+Score Dropdowns
-	    String languages[]={"University of Czechia","University of Denmark","University of Italy","University of Netherlands","University of Poland","University of Portugal","University of Spain"};
-	    //Todo ^^ getcountries list,calcucalte score
-	    JComboBox<String> jComboBox = new JComboBox<String>(languages);
-		final JComboBox<String> country1=jComboBox;    
-	    country1.setBounds(C_X,C1_Y,C_WIDTH,C_HEIGHT); 
-	    f.add(country1);
-	    final JComboBox<String> country2=jComboBox;    
-	    country2.setBounds(C_X,C2_Y,C_WIDTH,C_HEIGHT);
-	    f.add(country2);
-	    final JComboBox<String> country3=jComboBox;    
-	    country3.setBounds(C_X,C3_Y,C_WIDTH,C_HEIGHT); 
-	    f.add(country3);
+	    //University 1,2,3 Labels
+	    final JLabel uni1label = new JLabel();          
+	    uni1label.setHorizontalAlignment(JLabel.TRAILING);  
+	    uni1label.setSize(CL_X,CL1_Y);  
+	    uni1label.setText("Choice #1:");
+	    f.add(uni1label);
+	    final JLabel uni2label = new JLabel();          
+	    uni2label.setHorizontalAlignment(JLabel.TRAILING);  
+	    uni2label.setSize(CL_X,CL2_Y);  
+	    uni2label.setText("Choice #2:");
+	    f.add(uni2label);
+	    final JLabel uni3label = new JLabel();          
+	    uni3label.setHorizontalAlignment(JLabel.TRAILING);  
+	    uni3label.setSize(CL_X,CL3_Y);  
+	    uni3label.setText("Choice #3:");
+	    f.add(uni3label);
+	    
+	    
+	    
+	    
+	    //University+Score Drop-downs
+	    String universities[]={"---","University of Czechia","University of Denmark","University of Italy","University of Netherlands","University of Poland","University of Portugal","University of Spain"};
+		//TODO:
+	    //String universities[]={} (gets possible universities for student)
+	    //String languages[]={} for every university get's its language
+	    //Double scores[]={} for every university get's its score
+	    //then creates a "combined list" containing university + score
+	    //user chooses & when next's pressed we save that choice
+	    final JComboBox<String> uni1=new JComboBox<String>(universities);    
+	    uni1.setBounds(C_X,C1_Y,C_WIDTH,C_HEIGHT); 
+	    f.add(uni1);
+	    final JComboBox<String> uni2=new JComboBox<String>(universities); 
+	    uni2.setBounds(C_X,C2_Y,C_WIDTH,C_HEIGHT);
+	    f.add(uni2);
+	    final JComboBox<String> uni3=new JComboBox<String>(universities);  
+	    uni3.setBounds(C_X,C3_Y,C_WIDTH,C_HEIGHT); 
+	    f.add(uni3);
 	    
 	    //Next button
 	    JButton buttonNext=new JButton("Get Tips");  
@@ -60,8 +90,12 @@ public class UniversitySelectFrame {
 	    f.add(buttonNext);
 	    buttonNext.addActionListener(new ActionListener() {  
 	        public void actionPerformed(ActionEvent e) {
-	        	//tipsFrame();
-	        	//^^uncomment this when tipsFrame() is ready.
+	        	HashMap<University, Double> scores= new HashMap<University, Double>();
+	        	//TODO
+	        	//fill scores :) 
+	        	stud.setScores(scores);
+	        	//f.dispose();
+	        	//new tipsFrame(stud);
 	        }
 	    });
 	        
