31bfc166a3a5920a5d577980fdfb44091077e257
Romputer <constadine.petrides@gmail.com>
2019-06-08 13:27:09 +0300

    Moved createUnis from Main to CentralRegistry

--- src/CentralRegistry.java
+++ src/CentralRegistry.java
@@ -1,5 +1,6 @@
 import java.io.*;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class CentralRegistry
 {
@@ -101,6 +102,56 @@ public class CentralRegistry
 		public static ArrayList<Student> getUsers(){
 			
 			return s;
+			
+		}
+		
+		public static ArrayList<University> createUnis() {
+			
+			String name;
+			String town;
+			String country;
+			String language;
+			String line = null;
+			int nex = 0;
+			boolean langRequired;
+			ArrayList<University> uniList = new ArrayList<>();
+			ArrayList<Integer> coop;
+			University uni;
+			File f = new File("Unis.txt");
+			try {
+				
+				Scanner lineSc = new Scanner(f);
+				
+				
+				
+				while(lineSc.hasNext()) {
+					
+					line = lineSc.nextLine();
+					@SuppressWarnings("resource")
+					Scanner sc = new Scanner(line);
+					sc.useDelimiter(",");
+					
+					name = sc.next();
+					town = sc.next();
+					country = sc.next();
+					language = sc.next();
+					langRequired = sc.nextBoolean();
+					coop = new ArrayList<>();
+					while(sc.hasNextInt()) {
+						nex = sc.nextInt();
+						coop.add(nex);
+					}
+					uni = new University(name,town,country,language,coop,langRequired);
+					uniList.add(uni);
+				}
+				lineSc.close();
+				
+			}
+			
+			catch (FileNotFoundException e) {
+				e.printStackTrace();
+			}
+			return uniList;
 			
 		}
 		
--- src/Main.java
+++ src/Main.java
@@ -1,77 +1,29 @@
-import java.io.File;
-import java.io.FileNotFoundException;
 import java.util.ArrayList;
-import java.util.Scanner;
 
 public class Main {
 
 	
 	
 	public static void main(String[] args) {
-	ArrayList<University> uniList = new ArrayList<>();
-	ArrayList<Student> studentsList = new ArrayList<>();
-	uniList = createUnis();
-	CentralRegistry.createDep(uniList, studentsList);
-	
-	
-	CreateFile file = new CreateFile();
-	if(!file.checkFile()) {
-		file.newFile();
-	}
-	//new RegisterFrame();
-	
-	Student stud = new Student("user","cod");
-	stud.setDepartment(CentralRegistry.getDepartments().get(0));
-	new InputDataFrame(stud);
-	}
-	
-	private static ArrayList<University> createUnis() {
 		
-		String name;
-		String town;
-		String country;
-		String language;
-		String line = null;
-		int nex = 0;
-		boolean langRequired;
-		ArrayList<University> uniList = new ArrayList<>();
-		ArrayList<Integer> coop;
-		University uni;
-		File f = new File("Unis.txt");
-		try {
-			
-			Scanner lineSc = new Scanner(f);
-			
-			
-			
-			while(lineSc.hasNext()) {
-				
-				line = lineSc.nextLine();
-				@SuppressWarnings("resource")
-				Scanner sc = new Scanner(line);
-				sc.useDelimiter(",");
-				
-				name = sc.next();
-				town = sc.next();
-				country = sc.next();
-				language = sc.next();
-				langRequired = sc.nextBoolean();
-				coop = new ArrayList<>();
-				while(sc.hasNextInt()) {
-					nex = sc.nextInt();
-					coop.add(nex);
-				}
-				uni = new University(name,town,country,language,coop,langRequired);
-				uniList.add(uni);
-			}
-			lineSc.close();
-			
+		ArrayList<University> uniList;
+		ArrayList<Student> studentsList = new ArrayList<>();
+		uniList = CentralRegistry.createUnis();
+		CentralRegistry.createDep(uniList, studentsList);
+	
+	
+		/*CreateFile file = new CreateFile();
+		if(!file.checkFile()) {
+			file.newFile();
 		}
+		new RegisterFrame();
+	
+		Student stud = new Student("user","cod");
+		stud.setDepartment(CentralRegistry.getDepartments().get(0));
+		new InputDataFrame(stud);*/
 		
-		catch (FileNotFoundException e) {
-			e.printStackTrace();
-		}
-		return uniList;
-		
+		new SecretariatFrame();
 	}
+	
+	
 }
