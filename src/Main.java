b914f942d15ea051a3910b93e566e6b82bb00e75
Romputer <constadine.petrides@gmail.com>
2019-06-09 19:20:54 +0300

    Impoved ommunication between CentralRegistry and Main.

--- src/Main.java
+++ src/Main.java
@@ -1,35 +1,18 @@
-import java.util.ArrayList;
 
 public class Main {
-
-	
 	
 	public static void main(String[] args) {
 		
-		ArrayList<University> uniList;
-		ArrayList<Student> studentsList = new ArrayList<>();
-		uniList = CentralRegistry.createUnis();
-		CentralRegistry.createDep(uniList, studentsList);
-	
-		
-		
+		//Initialize Students
 		CreateFile file = new CreateFile();
-		if(!file.checkFile()) {
+		if(!file.checkFile())
 			file.newFile();
-		}
-		
-		//initialize list
 		CentralRegistry.desirializeUsers();
-		//strarting frame
+		
+		//Initialize Departments
+		CentralRegistry.createDep();
+		
+		//starting frame
 		new HomeFrame();
-		
-	
-	//	Student stud = new Student("user","cod");
-		//stud.setDepartment(CentralRegistry.getDepartments().get(0));
-	//	new InputDataFrame(stud);
-		
-		//new SecretariatFrame();
 	}
-	
-	
 }
