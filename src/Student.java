17818403b48f9ac9afb75a8ca2f72ddd8ab3c29c
Romputer <constadine.petrides@gmail.com>
2019-06-10 19:26:37 +0300

    Fixed a problem with copy function caused by the change to setDepartment.

--- src/Student.java
+++ src/Student.java
@@ -76,10 +76,14 @@ public class Student implements Serializable{
 		return this.langDegrees;
 	}
 	
+	public void setDepartmentForCopy(Department department) {
+		this.department = department;
+	}
+	
 	
 	public void copyTo(Student copy) {
 		copy.setScores(new HashMap<University,Double>(this.getScores()));
-		copy.setDepartment(this.getDepartment());
+		copy.setDepartmentForCopy(this.getDepartment());
 		copy.setAcademicYear(this.getAcademicYear());
 		copy.setAverageGrade(this.getAverageGrade());
 		copy.setnFailed(this.getnFailed());
